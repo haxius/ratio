28e0ebfdd3e50e1eadd976334da3b9ca685c171f
Randy Little <sevnsevn+github@gmail.com>
2021-05-20 19:45:42 +0000

    ADD boilerplate rendering layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 
+const _DELETE_ME = {
+  cards: [
+    {
+      "name": "Some Card",
+      "use": "Groceries",
+      "limit": 1250.00,
+      "ledger": [
+        {
+          "amount": 1.25,
+          "note": "Pear"
+        },
+        {
+          "amount": 2.25,
+          "note": "Bananas"
+        },
+        {
+          "amount": 9.95,
+          "note": "Steak"
+        }
+      ]
+    }
+  ]
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hi.
-      </header>
+    <div>
+      {_DELETE_ME.cards.map(({ name, use, limit, ledger }) => (
+        <React.Fragment key={name}>
+          <header>
+            <h1>{name}</h1>
+            <h4>Use: {use}</h4>
+          </header>
+          <p>Balance: {ledger.map(({ amount }) => amount).reduce((a, c) => a + c)}</p>
+          <p>Limit: {limit}</p>
+          <div>
+            {ledger.map(({ amount, note }, index) => (
+              <div key={`${name}-${index}`}>
+                <div>{amount}</div>
+                <div>{note}</div>
+              </div>
+            ))}
+          </div>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
